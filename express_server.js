30f4db12c28ce15a880d1a81a8eeb79f1973da22
vnctwong <vincentkywong@gmail.com>
2019-02-12 23:03:46 +0000

    properly calls urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs")
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -14,6 +16,13 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
