4025043afc9b1fec1b458bc732e913d9f9aaf9b0
vnctwong <vincentkywong@gmail.com>
2019-02-14 04:07:17 +0000

    cookie route working

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
-
+let express = require("express");
+let app = express();
+let PORT = 8080; // default port 8080
 app.set("view engine", "ejs")
+let cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
-var urlDatabase = {
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -25,7 +27,8 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let templateVars = {
-    urls: urlDatabase
+    urls: urlDatabase,
+    username: req.cookies["username"]
   };
   res.render("urls_index", templateVars);
 });
@@ -50,18 +53,29 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
 
-  var short = generateRandomString()
-  var long = req.body.longURL;
+  let short = generateRandomString()
+  let long = req.body.longURL;
   urlDatabase[short] = long; // Respond with 'Ok' (we will replace this)
   res.redirect('/urls/' + short); ///'urls/' + short
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
-  console.log('sdfeasdSfe')
   delete urlDatabase[req.params.shortURL];
-  console.log('sdfeasdSfe')
-  res.redirect('/urls')
+  res.redirect('/urls');
 })
+
+app.post('/urls/:shortURL', (req, res) => {
+
+  let shortURL = req.params.shortURL;
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL
+  res.redirect('/urls')
+});
+
+app.post('/login', (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
